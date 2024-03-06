eb43d91f3bb2eee007ba814534b5b42356dd50d3
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2024-03-05 23:09:33 -0300

    atualizando tabela apos remover itens

--- src/app/pages/budget/budget.component.ts
+++ src/app/pages/budget/budget.component.ts
@@ -51,6 +51,7 @@ export class BudgetComponent implements OnInit {
       .subscribe(
         (resultado: any) => {
           console.log(resultado);
+          this.fetchBudgets();
         },
         (erro: any) => {
           if (erro.status == 400) {
